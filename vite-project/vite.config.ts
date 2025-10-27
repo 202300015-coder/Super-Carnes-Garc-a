6bfa12048513ec6e159f2944a50a44dec3e9056c
202300015-Sergio <202300015@uprr.edu.mx>
2025-10-27 09:03:00 -0500

    aver

---
+++ vite-project/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  base:
+    process.env.NODE_ENV === "production" && process.env.GITHUB_REPOSITORY
+      ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
+      : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
